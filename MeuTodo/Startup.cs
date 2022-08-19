8f5ee67560dbb49de5d1a67ba00bd76c53b1e544
brenobarroso <barrososbreno@icloud.com>
2022-08-19 14:32:32 -0300

    update

--- MeuTodo/Startup.cs
+++ MeuTodo/Startup.cs
@@ -31,7 +31,10 @@ namespace MeuTodo
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(name: "default", pattern:"{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern:"{controller=Home}/{action=Index}/{id?}"
+                );
                 
             });
         }
