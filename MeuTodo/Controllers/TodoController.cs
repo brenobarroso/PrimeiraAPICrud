cefa9b4c2967d8f3e6b8e2a195ea79a55afaf66c
brenobarroso <barrososbreno@icloud.com>
2022-08-22 09:46:35 -0300

    leitura de um item do banco

--- MeuTodo/Controllers/TodoController.cs
+++ MeuTodo/Controllers/TodoController.cs
@@ -13,7 +13,7 @@ namespace MeuTodo.Controllers
     {
         [HttpGet]
         [Route("todos")]
-        public async Task<IActionResult> Get(
+        public async Task<IActionResult> GetAsync(
             [FromServices] AppDbContext context)
         {
             var todos = await context
@@ -21,6 +21,20 @@ namespace MeuTodo.Controllers
             .AsNoTracking()
             .ToListAsync();
             return Ok(todos);
+        }
+
+        [HttpGet]
+        [Route("todos/{id}")]
+        public async Task<IActionResult> GetByIdAsync(
+            [FromServices] AppDbContext context,
+            [FromRoute] int id)
+        {
+            var todo = await context
+            .Todos
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x=>x.Id == id);
+            return todo == null ? NotFound() : Ok(todo);
+            
         }
     }
 }
